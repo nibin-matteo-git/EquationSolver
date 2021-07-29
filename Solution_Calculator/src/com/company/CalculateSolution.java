4155606fac4e312fbfd813a099b6fc4db233585e
nibin-matteo-git <80332406+nibin-matteo-git@users.noreply.github.com>
2021-07-29 06:45:23 +0530

    third commit

--- Solution_Calculator/src/com/company/CalculateSolution.java
+++ Solution_Calculator/src/com/company/CalculateSolution.java
@@ -1,5 +1,13 @@
 package com.company;
 
 public class CalculateSolution {
-    
+    private static IUserInput inputObj;
+    private static int[] input1;
+    private static int[] input2;
+
+    public static int[] calculate(IUserInput inputObj){
+        this.inputObj=inputObj;
+        input1 = inputObj.getInput();
+        input2 = inputObj.getInput();
+    }
 }
